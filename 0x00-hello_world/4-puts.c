b4ab89beb31820583f5aaf0ee01915690338624e
bigfhame <echidennis2@gmail.com>
2022-12-12 16:49:02 -0800

    putin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 #include <string.h>
 void main () {
- char str[] 
+ char str[]; 
  strcpy (str, "Programming is like building a multilingual puzzle");
  puts(str);
  return 0;
 }
- 
+gcc 
