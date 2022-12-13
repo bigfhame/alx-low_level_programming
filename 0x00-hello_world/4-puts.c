a3ccb617e3071deb67407b191f58ecb43f2e0936
bigfhame <echidennis2@gmail.com>
2022-12-12 19:07:31 -0800

    donezo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 /*
- * main-entry point
- * 
- * return: always 0
- */
+*main-entry point
+*return: always 0
+*/
 int main(void)
-{	
+{
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
-}
+return (0); }
