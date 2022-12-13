d9ed3ebee343bff54cb85016503aa4cb4e58ec97
bigfhame <echidennis2@gmail.com>
2022-12-12 17:20:08 -0800

    please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-int main () {
- puts ("Programming is like building a multilingual puzzle");
+int main(void) {
+ puts (Programming is like building a multilingual puzzle);
  getchar();
  return 0;
 } 
