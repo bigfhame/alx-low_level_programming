e84f5891d75441aabc58c7622a0ecc52a1c50cd0
bigfhame <echidennis2@gmail.com>
2022-12-12 19:18:11 -0800

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-/*
-*main-entry point
-*return: always 0
+/**
+*main-Entry point
+*
+*Description:Using puts function
+*
+*Return: always 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); }
+return (0);
+}
