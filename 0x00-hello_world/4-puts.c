30f5aadfb25921178b3f84d9c239246193286a6b
bigfhame <echidennis2@gmail.com>
2022-12-12 17:42:51 -0800

    blast

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-int main(void) {
- puts (Programming is like building a multilingual puzzle);
- getchar();
+#include<stdio.h>
+int main() { 
+ puts("\"Programming is like building a multilingual puzzle\ ");
+ getchar;
  return 0;
-} 
+}
