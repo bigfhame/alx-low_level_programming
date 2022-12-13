1e418827b6c62edeae616e35d16a8aca28f82509
bigfhame <echidennis2@gmail.com>
2022-12-12 20:59:48 -0800

    hgi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("int is: %2d bytes\n", sizeof(int));
-printf("float is: %2d bytes\n", sizeof(float));
-printf("long int is: %2d bytes\n", sizeof(long int));
-printf("char is: %2d byte\n", sizeof(char))
-printf("long long int is: %2d bytes\n", sizeof(long long int));
+printf("int is an %2d bytes\n", sizeof(int));
+printf("float is a %2d bytes\n", sizeof(float));
+printf("long int is a %2d bytes\n", sizeof(long int));
+printf("char is a %2d byte\n", sizeof(char))
+printf("long long int is a %2d bytes\n", sizeof(long long int));
 return (0);
 }
 
