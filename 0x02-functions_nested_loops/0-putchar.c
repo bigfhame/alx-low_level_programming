0c47a20a5c8e1aa0da96c22f6ba64d35998110f0
bigfhame <echidennis2@gmail.com>
2022-12-13 19:54:12 -0800

    good

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - prints _putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{char ch = '_putchar';
+putchar(ch);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{char ch = '_putchar.c';
+putchar(ch);
+return (0);
+}
