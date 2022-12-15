ae0eadc7eb6aefabed107bbdbaaebe473105dce2
bigfhame <echidennis2@gmail.com>
2022-12-14 18:37:19 -0800

    dolo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - alphabet in lowercase
+*
+*Return: always 0
+*/
+int main(void)
+{char print_alphabet;
+for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
+{
+_putchar(print_alphabet); 
+}
+_putchar('\n');
+return (0);
+}
