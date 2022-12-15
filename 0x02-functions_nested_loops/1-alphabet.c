6081fbe9dc892080591d257416b26877ac2c2b19
bigfhame <echidennis2@gmail.com>
2022-12-14 19:33:28 -0800

    booyah

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
 *Return: always 0
 */
 int main(void)
-{char print_alphabet = 'a';
+{
+char print_alphabet;
 for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
 {
-_putchar(print_alphabet);
+putchar(print_alphabet);
 }
-_putchar('\n');
+putchar('\n');
 return (0);
 }
