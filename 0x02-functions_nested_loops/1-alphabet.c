25f0889688218ca9457faab1543c577e407fb694
bigfhame <echidennis2@gmail.com>
2022-12-14 18:55:50 -0800

    dodge

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 *Return: always 0
 */
 int main(void)
-{char print_alphabet;
+{char print_alphabet = 'a';
 for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
 {
-_putchar(print_alphabet); 
+_putchar(print_alphabet);
 }
 _putchar('\n');
 return (0);
