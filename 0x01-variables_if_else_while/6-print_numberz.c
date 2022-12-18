533479ccc944d3c271bd097dd7a1c206a1142487
bigfhame <echidennis2@gmail.com>
2022-12-17 16:06:47 -0800

    fed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*
+*Return: always 0
+*/
+int main(void)
+{char a = '0';
+while (a < 10)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
