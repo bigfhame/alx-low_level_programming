f6f58642e267a458c669284b45ec0219eeab4957
bigfhame <echidennis2@gmail.com>
2022-12-27 16:19:45 -0800

    suh

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - check for lowercase character
+*
+*Return: always 0
+*/
+int _islower(int c)
+{
+int l;
+if ( _islower(l) )
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
