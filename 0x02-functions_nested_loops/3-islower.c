3ff44d9e1a42bc6ec3e4ea6c87cbee3cbc93f8fe
bigfhame <echidennis2@gmail.com>
2022-12-29 09:02:21 -0800

    next

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 /**
 *_islower - check for lowercase character
-*int c - character to be accessed
+*r
 *Return: always 0
 */
 int _islower(int c)
 {
-	 c = 'H';
-if (_islower(c))
+c = 65;
+if (islower(c))
 {
-	_putchar(1);
+return (1);
 }
 else
 {
-_putchar(0);
+return (0);
 }
+}
+/**
+ *main - Entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+int po;
+po = _islower('H');
+_putchar(po + '0');
 return (0);
 }
