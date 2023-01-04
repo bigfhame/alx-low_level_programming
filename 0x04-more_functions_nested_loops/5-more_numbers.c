c300617d8f698f039b0d68bb3921f469391b192b
bigfhame <echidennis2@gmail.com>
2023-01-04 11:01:01 -0800

    ass

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *more_numbers - numbers
+ *
+ *Return: always 0
+ */
+void more_numbers(void)
+{
+	int p;
+	char multi;
+	for (p = 0; p <= 14; p++)
+	{
+		for (multi = 'a'; multi <= 'z'; multi++)
+		_putchar(multi);
+		_putchar("\n");
+	}
+}
