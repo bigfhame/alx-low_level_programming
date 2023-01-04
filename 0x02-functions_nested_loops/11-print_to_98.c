58d914da4c08712dd22c106d8e004ccac45dd3fd
bigfhame <echidennis2@gmail.com>
2023-01-04 01:34:15 -0800

    joy

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_to_98 - Entry point
+ *@n: natural numbers
+ *Return: always 0
+ */
+void print_to_98(int n);
+{
+n = 0;
+while  (n <= 98)
+{
+_putchar(n + '0');
+n++;
+_putchar(',');
+_putchar(' ');
+}
+return (0);
+}
