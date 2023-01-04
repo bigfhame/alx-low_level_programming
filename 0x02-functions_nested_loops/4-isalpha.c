9bf038b78a122421acaf3999cfacbfa457228a4c
bigfhame <echidennis2@gmail.com>
2023-01-04 11:42:56 -0800

    val

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *_isalpha - checks for alphabet
+ *
+ *Return: always 0
+ */
+int _isalpha(int c)
+{
+	if (c =
+}
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,29 +2,10 @@
 #include <stdlib.h>
 /**
  *_abs - absolute value of integer
- *parameter - int
+ *@N: int
  *Return: always 0
  */
-int _abs(int)
+int _abs(int N)
 {
-printf("%d\n", abs(int));
-return (0);
-}
-/**
- *main - Entry point
- *
- * Return: always (0)
- */
-int main(void)
-{
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+return ((N < 0) ? (-N) : (N));
 }
