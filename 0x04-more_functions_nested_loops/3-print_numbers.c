c0aef4bc347f3f899cc65c2cd875041af730acac
bigfhame <echidennis2@gmail.com>
2023-01-05 06:33:33 -0800

    oye

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
  */
 void print_numbers(void)
 {
-	int n = 0;
+	int n;
 
-	while (n <= 9)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(n + '0');
-		n++;
+	_putchar(n + '0');
 	}
 	_putchar("\n");
 }
