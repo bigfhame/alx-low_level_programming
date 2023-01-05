94d4b3c9ddc7abf0498a910b1675e52f4ee72a34
bigfhame <echidennis2@gmail.com>
2023-01-05 06:17:01 -0800

    flash

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,36 @@
  */
 void print_to_98(int n)
 {
-n = 0;
-while (n <= 98)
-{
-_putchar(n + '0');
-n++;
-}
-_putchar(',');
-_putchar(' ');
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
