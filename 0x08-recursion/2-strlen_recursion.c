20665775f4b19b6468ff55a7c42cfb4730b8a0ce
bigfhame <echidennis2@gmail.com>
2023-01-05 22:47:38 -0800

    lasy

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen_recursion - string lenght
+ *@s: string lenght to access
+ *
+ *Return: an int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
