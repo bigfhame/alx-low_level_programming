04a9bb0bf41a6731108fd385ddb1b7b8c4754523
bigfhame <echidennis2@gmail.com>
2023-01-06 06:33:32 -0800

    sadd

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *is_palindrome - function to check
+ *@*s: string to check
+ *
+ *Return: a string
+ */
+
+int is_palindrome(char *s)
+{
+	if (strlen(s) == 0)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
