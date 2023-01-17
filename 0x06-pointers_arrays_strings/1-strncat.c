dd579cbc75c8c95d3bce75add22def0e740f1993
Echi Dennis <echidennis2@gmail.com>
2023-01-17 02:31:03 -0800

    dest

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  *_strncat - concatenates two strings using at most
- an inputted number of bytes from src.
+ * an inputted number of bytes from src.
  *
  *@dest: the string to be appended upon.
  *
