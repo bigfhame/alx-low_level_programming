00c97d7625f115aae725c64e562a7c4b33ceaa6b
Echi Dennis <echidennis2@gmail.com>
2023-01-17 21:00:05 -0800

    gag

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ *init_dog - initializes a dog structure
+ *@d: dog structure
+ *@name: n
+ *@age: age
+ *@owner: owner
+ *Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
