32e9c54b79aa7cc4c59a27683d65eb9597a1e3eb
Bigfhame <echidennis2@gmail.com>
2023-01-18 20:28:06 -0800

    gaga

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include "stdlib.h"
+/**
+ *print_name - prints name
+ *@name: name to print
+ *@f: pointer to function
+ *
+ *Return: Nothin
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
