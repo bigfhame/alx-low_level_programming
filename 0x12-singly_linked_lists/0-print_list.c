50a55868606f1f63978c9ab8ed161cb5a669c07e
bigfhame <echidennis2@gmail.com>
2023-01-29 10:48:26 -0800

    loppy

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- *print_list - print a linked list
- *
- *@h: the pointer to the list h
- *Return: number of nodes
- */
 
+/**
+ * print_list - print a linked list
+ * @h: the pointer to the list h
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
@@ -16,8 +15,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-		else
+		}
+			else
 		{
 			printf("[%d] (%s)\n", h->len, h->str);
 		}
